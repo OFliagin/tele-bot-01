675dd4e22139aff40f43dfdd892635c8dc5fe19c
Oleksandr Fliagin <oleksamdr.fliagin@ardas.com>
2020-12-20 15:23:14 +0200

    refactored names in handler

--- src/main/java/com/example/tele/bot/service/LocalMessageService.java
+++ src/main/java/com/example/tele/bot/service/LocalMessageService.java
@@ -16,15 +16,15 @@ public class LocalMessageService {
     private final MessageSource messageSource;
 
     @Autowired
-    public LocalMessageService(@Value("${bot.locale.lang:ru-RU}") String localeTag, MessageSource messageSource) {
+    public LocalMessageService(@Value("${localeTag}") String localeTag, MessageSource messageSource) {
         this.locale = Locale.forLanguageTag(localeTag);
         this.messageSource = messageSource;
     }
 
     public String getMessage(String message) {
-        log.debug("get message [{}] for local {}", message ,locale.getLanguage());
+        log.debug("get message [{}] for local {}", message, locale.getLanguage());
         String res = messageSource.getMessage(message, null, locale);
-        log.debug("got message [{}] for local {}", res ,locale.getLanguage());
+        log.debug("got message [{}] for local {}", res, locale.getLanguage());
         return res;
     }
 
